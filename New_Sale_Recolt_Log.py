d5c74eb4ed88e4810e3e70192f8cd47a8feeb17d
Yas2303 <germarocservices.com>
2025-06-14 01:18:33 +0200

    Init Firset depo

--- New_Sale_Recolt_Log.py
+++ New_Sale_Recolt_Log.py
@@ -36,7 +36,6 @@ def display_sales_graphs(df):
     if df.empty:
         st.info("Aucune donnée de vente disponible.")
         return
-
     
     st.markdown(
         f"<h2 style='color: #007bad;text-align: left;'>Statistique de Vente : {st.session_state.get('username', )}</h2>",
@@ -51,14 +50,13 @@ def display_sales_graphs(df):
     col1, col2, col3 = st.columns(3)
     kpi_card_html(col1, "Total Ventes", f"{total_sales:,.2f}€", "#007bad", "money-bill-wave")
     kpi_card_html(col2, "Moyenne/vente", f"{avg_sale:,.2f}€", "#00afe1", "chart-line")
-    kpi_card_html(col3, "Note moyenne", f"{avg_rating:.1f}/9", "#ffc107", "star")
+    kpi_card_html(col3, "Note moyenne", f"{avg_rating:.1f}/9", "#ffc107", "smile")
 
     # Graphs
     st.markdown("---")
     col1, col2 = st.columns(2)
 
     with col1:
-        
         st.markdown("**Ventes par Ville**")
         df_city = df.groupby('City')['Total_Sale'].sum().reset_index()
         fig_city = px.bar(df_city, 
@@ -83,7 +81,7 @@ def display_sales_graphs(df):
             yaxis={'categoryorder': 'total ascending'},
             showlegend=False,
             margin=dict(l=20, r=20, t=40, b=20),  # Ajuster les marges si nécessaire
-            height=500                            # Hauteur fixe pour mieux voir
+            height=500                           # Hauteur fixe pour mieux voir
         )
         
         st.plotly_chart(fig_city, use_container_width=True)
@@ -94,7 +92,7 @@ def display_sales_graphs(df):
             df['ORDER_DATE'] = pd.to_datetime(df['ORDER_DATE'])
             df_date = df.groupby(df['ORDER_DATE'].dt.date)['Total_Sale'].sum().reset_index()
             fig_date = px.line(df_date, x='ORDER_DATE', y='Total_Sale'
-                               )
+                              )
             fig_date.update_xaxes(title_text='Date')
             fig_date.update_yaxes(title_text='Total Ventes (€)')
             # Add data labels for line chart
@@ -130,7 +128,6 @@ def display_recolt_graphs(df):
     col1, col2 = st.columns(2)
 
     with col1:
-       
         st.markdown("**Récoltes par Ville**")
         df_city = df.groupby('City')['Total_Recolt'].sum().reset_index()
         fig_city = px.bar(
@@ -140,7 +137,6 @@ def display_recolt_graphs(df):
             orientation='h',
             color='City',
             text='Total_Recolt'  # Étiquettes de données
-            #title='Récoltes par Ville (en €)'
         )
         
         # Personnalisation des étiquettes
@@ -184,7 +180,7 @@ def display_recolt_graphs(df):
                 df['ORDER_DATE'] = pd.to_datetime(df['ORDER_DATE'])
                 df_date = df.groupby(df['ORDER_DATE'].dt.date)['Total_Recolt'].sum().reset_index()
                 fig_date = px.line(df_date, x='ORDER_DATE', y='Total_Recolt',
-                                   title='Évolution des récoltes au fil du temps')
+                                  title='Évolution des récoltes au fil du temps')
                 fig_date.update_xaxes(title_text='Date')
                 fig_date.update_yaxes(title_text='Total Récoltes (€)')
                 fig_date.update_traces(mode='lines+markers+text', text=df_date['Total_Recolt'].apply(lambda x: f'{x:,.2f}€'), textposition='top center')
@@ -200,7 +196,7 @@ def display_recolt_graphs(df):
         df['ORDER_DATE'] = pd.to_datetime(df['ORDER_DATE'])
         df_date_overall = df.groupby(df['ORDER_DATE'].dt.date)['Total_Recolt'].sum().reset_index()
         fig_recolt_line_overall = px.line(df_date_overall, x='ORDER_DATE', y='Total_Recolt',
-                               title='Évolution Générale des Récoltes au fil du temps')
+                                  title='Évolution Générale des Récoltes au fil du temps')
         fig_recolt_line_overall.update_xaxes(title_text='Date')
         fig_recolt_line_overall.update_yaxes(title_text='Total Récoltes (€)')
         fig_recolt_line_overall.update_traces(mode='lines+markers+text', text=df_date_overall['Total_Recolt'].apply(lambda x: f'{x:,.2f}€'), textposition='top center')
@@ -209,6 +205,26 @@ def display_recolt_graphs(df):
     except Exception:
         st.warning("Données de date indisponibles pour l'évolution générale des récoltes.")
 
+def get_unique_values_from_table(table_name, column_name):
+    """Récupère les valeurs uniques d'une colonne spécifique d'une table"""
+    conn = get_db_connection()
+    try:
+        query = f"SELECT DISTINCT {column_name} FROM {table_name} WHERE {column_name} IS NOT NULL"
+        df = pd.read_sql(query, conn)
+        return df[column_name].tolist()
+    except Exception as e:
+        st.error(f"Erreur lors de la récupération des données: {e}")
+        return []
+    finally:
+        if conn:
+            conn.close()
+
+def clear_form():
+    """Vide tous les champs du formulaire"""
+    keys_to_clear = [key for key in st.session_state.keys() 
+                    if key.startswith('sale_') or key.startswith('recolt_')]
+    for key in keys_to_clear:
+        del st.session_state[key]
 
 def New_Sale_Agent():
     col1, col2 = st.columns([2, 2])
@@ -239,15 +255,25 @@ def New_Sale_Agent():
     )
 
     try:
+        # Récupérer les valeurs uniques depuis la table Sales
+        countries = get_unique_values_from_table('Sales', 'Country')
+        cities = get_unique_values_from_table('Sales', 'City')
+
         # Formulaire de vente
         col1, col2, col3 = st.columns([1, 2, 2])
         with col1:
             st.markdown("### Numéro du BP")
             order_reference = st.text_input("Order Reference", key="sale_ref")
             order_date = st.date_input("Order Date", value=datetime.today(), key="sale_date")
-            country = st.selectbox("Country", ["Autriche", "France"], key="sale_country")
-            cities = ["Vienne", "Graz", "Linz", "Salzbourg"] if country == "Autriche" else ["Paris", "Lyon", "Marseille", "Toulouse"]
-            city = st.selectbox("City", cities, key="sale_city")
+            country = st.selectbox("Country", countries, key="sale_country")
+            
+            # Filtrer les villes en fonction du pays sélectionné
+            selected_country = st.session_state.get('sale_country', None)
+            if selected_country:
+                cities_in_country = get_unique_values_from_table('Sales', f"City WHERE Country = '{selected_country}'")
+                city = st.selectbox("City", cities_in_country, key="sale_city")
+            else:
+                city = st.selectbox("City", cities, key="sale_city")
 
         with col2:
             st.markdown("### Montant (€)")
@@ -267,7 +293,7 @@ def New_Sale_Agent():
                     <style>
                         div.stButton > button {
                             width: 100%;
-                            background-color: #007bad;
+                            background-color: #fcce22;
                             color: white;
                             border-radius: 5px;
                             border: none;
@@ -275,7 +301,7 @@ def New_Sale_Agent():
                             font-size: 1.1em;
                         }
                         div.stButton > button:hover {
-                            background-color: #005f82;
+                            background-color: #fcce22;
                         }
                     </style>
                     """, unsafe_allow_html=True)
@@ -299,11 +325,11 @@ def New_Sale_Agent():
                         st.error(f"Erreur: {str(e)}")
 
             with annuler:
-                    st.markdown("""
+                st.markdown("""
                     <style>
                         div.stButton > button {
                             width: 100%;
-                            background-color: #e0e0e0;
+                            background-color: #fcce22;
                             color: #333;
                             border-radius: 5px;
                             border: none;
@@ -311,13 +337,13 @@ def New_Sale_Agent():
                             font-size: 1.1em;
                         }
                         div.stButton > button:hover {
-                            background-color: #c0c0c0;
+                            background-color: #fcce22;
                         }
                     </style>
                     """, unsafe_allow_html=True)
-
-                    if st.button("**Annuler**", key="cancel_sale"):
-                        st.warning("Opération annulée")
+                if st.button("**Annuler**", key="cancel_sale", use_container_width=True):
+                    clear_form()
+                    st.warning("Opération annulée et champs vidés")
         with col3:
             st.markdown("### Informations sur le Log :")
         # Afficher les graphiques après le formulaire
@@ -358,18 +384,28 @@ def New_Recolt_Agent():
     )
 
     try:
+        # Récupérer les valeurs uniques depuis la table Recolt
+        countries = get_unique_values_from_table('Recolt', 'Country')
+        cities = get_unique_values_from_table('Recolt', 'City')
+        banks = get_unique_values_from_table('Recolt', 'Banques')
+
         # Formulaire de récolte
         col1, col2, col3 = st.columns([1, 2, 2])
         with col1:
             st.markdown("### Numéro du BP")
             order_reference = st.text_input("Order Reference", key="recolt_ref")
             order_date = st.date_input("Order Date", value=datetime.today(), key="recolt_date")
-            country = st.selectbox("Country", ["Autriche", "France"], key="recolt_country")
-            cities = ["Vienne", "Graz", "Linz", "Salzbourg"] if country == "Autriche" else ["Paris", "Lyon", "Marseille", "Toulouse"]
-            city = st.selectbox("City", cities, key="recolt_city")
-            # Added a placeholder for Bank selection. You'll need to populate this based on your data source.
-            bank = st.selectbox("Banque", ["Banque A", "Banque B", "Banque C", "Autres"], key="recolt_bank")
-
+            country = st.selectbox("Country", countries, key="recolt_country")
+            
+            # Filtrer les villes en fonction du pays sélectionné
+            selected_country = st.session_state.get('recolt_country', None)
+            if selected_country:
+                cities_in_country = get_unique_values_from_table('Recolt', f"City WHERE Country = '{selected_country}'")
+                city = st.selectbox("City", cities_in_country, key="recolt_city")
+            else:
+                city = st.selectbox("City", cities, key="recolt_city")
+            
+            bank = st.selectbox("Banque", banks, key="recolt_bank")
 
         with col2:
             st.markdown("### Montant (€)")
@@ -389,7 +425,7 @@ def New_Recolt_Agent():
                     <style>
                         div.stButton > button {
                             width: 100%;
-                            background-color: #007bad;
+                            background-color: #fc6c04;
                             color: white;
                             border-radius: 5px;
                             border: none;
@@ -397,7 +433,7 @@ def New_Recolt_Agent():
                             font-size: 1.1em;
                         }
                         div.stButton > button:hover {
-                            background-color: #005f82;
+                            background-color: #fc6c04;
                         }
                     </style>
                     """, unsafe_allow_html=True)
@@ -407,7 +443,7 @@ def New_Recolt_Agent():
                         cursor = conn.cursor()
                         cursor.execute(
                             """INSERT INTO Recolt (Hyp, ORDER_REFERENCE, ORDER_DATE, SHORT_MESSAGE, Country, City, Total_Recolt, Rating, Banques)
-                            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)""", # Corrected column name to 'Banques'
+                            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)""",
                             (hyp_agent, order_reference, order_date, short_message, country, city, total_recolt, rating, bank)
                         )
                         conn.commit()
@@ -427,7 +463,7 @@ def New_Recolt_Agent():
                     <style>
                         div.stButton > button {
                             width: 100%;
-                            background-color: #e0e0e0;
+                            background-color: #fcce22;
                             color: #333;
                             border-radius: 5px;
                             border: none;
@@ -435,12 +471,13 @@ def New_Recolt_Agent():
                             font-size: 1.1em;
                         }
                         div.stButton > button:hover {
-                            background-color: #c0c0c0;
+                            background-color: #fcce22;
                         }
                     </style>
                     """, unsafe_allow_html=True)
                 if st.button("**Annuler**", key="cancel_recolt"):
-                    st.warning("Opération annulée")
+                    clear_form()
+                    st.warning("Opération annulée et champs vidés")
 
             # Afficher les graphiques après le formulaire
         display_recolt_graphs(agent_recolts)
